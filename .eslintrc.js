6f1531ea7fb5d1934fda1190a0f6ccf02eb39c9f
jinke <1359518268@qq.com>
2018-03-01 17:10:18 +0800

    chore:init commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    parserOptions: {
+      ecmaVersion: 2017,
+      ecmaFeatures: {
+        impliedStrict: true,
+      },
+    },
+    extends: ["eslint:recommended", "prettier"],
+    env: {
+      es6: true,
+      node: true,
+    },
+    rules: {
+      "object-shorthand": "error",
+      camelcase: ["error", { properties: "never" }],
+      eqeqeq: ["error", "smart"],
+      "linebreak-style": ["error", "unix"],
+      "new-cap": "error",
+      "no-array-constructor": "error",
+      "no-console": "off",
+      "no-lonely-if": "error",
+      "no-loop-func": "error",
+      "no-param-reassign": "error",
+      "no-sequences": "error",
+      "no-shadow-restricted-names": "error",
+      "no-unneeded-ternary": "error",
+      "no-unused-expressions": "error",
+      "no-unused-vars": ["error", { args: "none" }],
+      "no-use-before-define": ["error", "nofunc"],
+      "no-var": "error",
+      "prefer-arrow-callback": "error",
+      "prefer-spread": "error",
+      "prefer-template": "error",
+      "wrap-iife": ["error", "inside"],
+      yoda: ["error", "never"],
+    },
+  }
